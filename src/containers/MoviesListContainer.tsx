04fa57bb541e30c226139adfc0031bbe7430c2ca
Daniel Simonov <sk8kmd@gmail.com>
2020-10-28 22:09:42 +0300

    Add @types/react-redux, add prototype of first container component

---
+++ src/containers/MoviesListContainer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {connect} from 'react-redux';
+
+const mapStateToProps = () => {
+
+}
+
+const mapDispatchToProps = () => {
+
+}
+
+function MoviesListContainer(props){
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesListContainer);
