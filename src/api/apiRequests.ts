5dfebe114ba00d57439984905dbe466d8e12c321
Daniel Simonov <sk8kmd@gmail.com>
2020-11-01 18:09:24 +0300

    Refactor api requests, add new endpoint

--- src/api/apiRequests.ts
+++ src/api/apiRequests.ts
@@ -1,11 +1,25 @@
 import axios from 'axios';
 
-const createURL = (endpoint: string, page = 1): string => {
+const POPULAR = '/movie/popular';
+const SEARCH = '/search/movie';
+
+const createURL = (endpoint: string, page = 1, query?: string): string => {
     const URL = 'https://api.themoviedb.org/3';
     const KEY = '8282c68f5ed8f63c5bfae413614846d5';
-    return `${URL}${endpoint}?api_key=${KEY}&language=en-US&page=${page}`;
+    switch (endpoint){
+        case POPULAR:
+            return `${URL}${endpoint}?api_key=${KEY}&language=en-US&page=${page}`;
+        case SEARCH:
+            return `${URL}${endpoint}?api_key=${KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`
+        default:
+            return `${URL}${POPULAR}?api_key=${KEY}&language=en-US&page=${page}`;
+    }
 }
 
 export const getPopularMoviesReq = (page?: number) => axios
-    .get(createURL('/movie/popular', page))
+    .get(createURL(POPULAR, page))
+        .then(res => res.data);
+
+export const getSearchMoviesReq = (page?: number, query?: string) => axios
+    .get(createURL(SEARCH, page, query))
         .then(res => res.data);
