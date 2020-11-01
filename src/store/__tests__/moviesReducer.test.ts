b1585507b5ecf50c5c92819bd35e011e3b380593
Daniel Simonov <sk8kmd@gmail.com>
2020-11-01 19:01:49 +0300

    Refactor movies reducer tests

--- src/store/__tests__/moviesReducer.test.ts
+++ src/store/__tests__/moviesReducer.test.ts
@@ -12,24 +12,24 @@ let newState = (state: MoviesState, action: MoviesActionTypes) => {
     return moviesReducer(state, action);
 }
 
-describe('movies reducer', () => {
-    test('set popular movies in state', async () => {
-        const payload: MoviesPayload = await getPopularMoviesReq();
-        const action = getPopularMoviesCreator(payload);
-        expect(newState(initialState, action).movies).toStrictEqual(payload.results);
-    });
+test('set popular movies in state', async () => {
+    const payload: MoviesPayload = await getPopularMoviesReq();
+    const action = getPopularMoviesCreator(payload);
+    expect(newState(initialState, action).movies).toStrictEqual(payload.results);
+});
 
-    test('set searched movies in state', async () => {
-        const payload: MoviesPayload = await getSearchMoviesReq(1,'game');
-        const action = getSearchMoviesCreator(payload);
-        expect(newState(initialState, action).movies).toStrictEqual(payload.results);
-    });
+test('set searched movies in state', async () => {
+    const action = clearMoviesCreator();
+    newState(initialState, action);
+    const payload: MoviesPayload = await getSearchMoviesReq(1, 'game');
+    const action2 = getSearchMoviesCreator(payload);
+    expect(newState(initialState, action2).movies).toStrictEqual(payload.results);
+});
 
-    test('clear movies in state', async () => {
-        const payload: MoviesPayload = await getSearchMoviesReq(1,'game');
-        const action = getSearchMoviesCreator(payload);
-        newState(initialState, action);
-        const action2 = clearMoviesCreator();
-        expect(newState(initialState, action2).movies).toStrictEqual([]);
-    });
+test('clear movies in state', async () => {
+    const payload: MoviesPayload = await getSearchMoviesReq(1, 'game');
+    const action = getSearchMoviesCreator(payload);
+    newState(initialState, action);
+    const action2 = clearMoviesCreator();
+    expect(newState(initialState, action2).movies).toStrictEqual([]);
 });
